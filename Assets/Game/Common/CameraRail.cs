f2713c8bb2d629602cff2cbea35750962059ce47
Oliver Perez Camargo <oliver.a.perez.c@gmail.com>
2016-05-31 22:13:25 -0500

    Add camera movement

---
+++ Assets/Game/Common/CameraRail.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+
+public class CameraRail : MonoBehaviour {
+
+	[SerializeField]
+	private PlayerMain target;
+	
+	[SerializeField]
+	private float minSpeed = 0f;
+	[SerializeField]
+	private float speedFactor = 0.025f;
+	
+	private Vector3 basePosition;
+
+	// Use this for initialization
+	void Awake () {
+		if(target == null) throw new Exception("target not set");
+		basePosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distanceSqr = Vector3.SqrMagnitude(transform.position - target.transform.position);
+		float speed = Mathf.Max(minSpeed, distanceSqr * speedFactor);
+
+		basePosition = Vector3.MoveTowards(basePosition, target.transform.position, speed);
+
+		transform.position = basePosition;
+	}
+}
