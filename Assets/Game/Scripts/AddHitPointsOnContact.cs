66afb6a86c3bc687f3930a470482e563c651a187
Oliver Perez Camargo <oliver.a.perez.c@gmail.com>
2016-06-14 20:54:36 -0500

    Improved test scene 1
    Add a boss on the first test scene
    Add dead zone at the bottom of the scenario

---
+++ Assets/Game/Scripts/AddHitPointsOnContact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+
+public class AddHitPointsOnContact : MonoBehaviour {
+
+	[SerializeField]
+	private CollisionPub collisionPub;
+
+	[SerializeField]
+	private float damage = 0;
+
+	[SerializeField]
+	private bool destroyOnContact = false;
+
+	void Awake() {
+		if(collisionPub == null) throw new Exception("collisionPub not defined");
+
+		collisionPub.OnTriggerEnterEvent += TriggerEnter;
+	}
+
+	void TriggerEnter(Collider other) {
+		PlayerConstitution player = other.GetComponent<PlayerConstitution>();
+		if(player != null)  {
+			player.AddHitPoints(damage);
+			if(destroyOnContact) gameObject.DestroyAPS();
+		}
+	}
+}
