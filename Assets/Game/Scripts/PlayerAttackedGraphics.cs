c4ed25bf1e7bcb40ea19b68fbd4e04d0398ddca5
Oliver Pérez <oliver.a.perez.c@gmail.com>
2016-07-23 22:10:56 -0500

    Add enemies with animations and abstract avatars

--- Assets/Game/Scripts/PlayerAttackedGraphics.cs
+++ Assets/Game/Scripts/PlayerAttackedGraphics.cs
@@ -3,34 +3,25 @@ using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerConstitution))]
+[RequireComponent(typeof(Animator))]
 public class PlayerAttackedGraphics : MonoBehaviour {
 
     private PlayerConstitution playerConstitution;
-    
-    private ParticleSystem[] chargingPS;
+    private Animator animator;
 
-    [SerializeField]
-    private Animator _animation;
-
-    // private bool active = false;
-    
-	void Awake () {
+    void Awake()
+    {
         playerConstitution = GetComponent<PlayerConstitution>();
-        if (_animation == null) throw new Exception("animation not set");
+        animator = GetComponent<Animator>();
     }
-	void Start () {
+
+    void Start()
+    {
         playerConstitution.OnAttackedEvent += Attacked;
     }
-	
-    void Attacked(PlayerMain playerMain, PlayerWeaponDef weapon) {
-        _animation.SetTrigger("attacked");
-    }
 
-    IEnumerator Play() {
-        _animation.enabled = false;
-        yield return new WaitForEndOfFrame();
-        _animation.enabled = true;
+    void Attacked(PlayerMain playerMain, PlayerWeaponDef weapon)
+    {
+        animator.SetTrigger("stunned");
     }
-    
-	void Update () {}
 }
